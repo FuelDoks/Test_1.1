e9d71f568631d7cc066d0e751024559080187444
FuelDoks <fueldoks@gmail.com>
2016-10-14 19:12:58 +0400

    Initial commit

---
+++ src/Word_1/Text.java
@@ -0,0 +1,33 @@
+package Word_1;
+
+/**
+ * Created by Dick_in_your_hand on 14.10.2016.
+ */
+import java.io.*;
+public class Text {
+
+    public static void main(String args[])  throws IOException {
+        FileInputStream in  = null;
+        FileOutputStream    = null;
+
+        
+        try {
+            in = new FileInputStream("input.txt");
+            Out = new FileOutputStream("output.txt");
+
+            int c;
+            while ((c =in.read())   !=  -1) {
+                out.write(c);
+            }
+        }finally {
+            if (in  !=null) {
+                in.close();
+            }
+            if (out !=null) {
+                out.close();
+            }
+        }
+
+    }
+
+}
