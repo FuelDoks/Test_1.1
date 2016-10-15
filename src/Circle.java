83e9e3aef07e28062387426aafc482e7fc122025
FuelDoks <fueldoks@gmail.com>
2016-10-15 15:34:04 +0300

    Check it

---
+++ src/Circle.java
@@ -0,0 +1,24 @@
+public class Circle {
+
+    float x;
+    float y;
+    float radius;
+
+    Circle CircleA = new Circle();
+    CircleA.x = 4;
+    CircleA.y = 2;
+    CircleA.radius = 1;
+
+    Circle CircleB = new Circle();
+    CircleB.x = 42;
+    CircleB.y = 33;
+    CircleB.radius = 20;
+
+    Circle CircleC = new Circle();
+    CircleC.x = 22;
+    CircleC.y = 11;
+    CircleC.radius = 5;
+
+
+
+}
